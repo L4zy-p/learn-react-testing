665fb278a7f1e2a997c8c3b10ff56d2669170930
L4zy-p <hemhongsa.p94@gmail.com>
2021-12-05 16:45:00 +0700

    [initial]: initial exist

--- src/components/Avatar.test.js
+++ src/components/Avatar.test.js
@@ -6,8 +6,27 @@ describe("Avatar", () => {
   it("should render element with role='img'", () => {
     render(<Avatar />);
 
-    expect(screen.getByRole("img")).toBeDefined();
+    // expect(screen.queryAllByRole("img")).toBeDefined(); // จะ return HTMLElement || null อาจจะใช้ในกรณีที่กด cliCk แล้วหายไป
+    expect(screen.getByRole("img")).toBeDefined(); // จะ return HTMLElement || throw error อาจจะใช้ในกรณ๊ที่ Component บึ้ม
   });
+
+  it("should have src inside element", () => {
+    render(<Avatar src='https://test.com' />);
+
+    expect(screen.getByRole("img")).toHaveAttribute('src', 'https://test.com')
+  });
+
+  it("should render icon if no src provided", () => {
+    render(<Avatar />);
+
+    expect(screen.getByRole("img").firstChild.nodeName).toEqual('svg')
+  })
+
+  it("should be able to replace default icon with children", () => {
+    render(<Avatar>L4zy-p</Avatar>)
+
+    expect(screen.getByRole('img').textContent).toEqual('L4zy-p')
+  })
 
   it("should spread aria-label to root element", () => {
     render(<Avatar aria-label="Jun" />);
@@ -15,6 +34,7 @@ describe("Avatar", () => {
     expect(screen.getByLabelText("Jun")).toBeDefined();
   });
 
+  // test style props ข้างใน
   it("should receive style prop", () => {
     render(
       <Avatar aria-label="Jun" style={{ fontVariant: "no-contextual" }} />
@@ -23,26 +43,5 @@ describe("Avatar", () => {
     expect(screen.getByLabelText("Jun")).toHaveStyle({
       "font-variant": "no-contextual",
     });
-  });
-
-  it("should have src inside element", () => {
-    render(<Avatar src="https://something.com" />);
-
-    expect(screen.getByRole("img")).toHaveAttribute(
-      "src",
-      "https://something.com"
-    );
-  });
-
-  it("should render icon if no `src` provided", () => {
-    render(<Avatar />);
-
-    expect(screen.getByRole("img").firstChild.nodeName).toEqual("svg");
-  });
-
-  it("should be able to replace default icon with children", () => {
-    render(<Avatar>SK</Avatar>);
-
-    expect(screen.getByRole("img").textContent).toEqual("SK");
   });
 });
